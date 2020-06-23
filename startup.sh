5bc66e60686ae313d4ca8c2c677de17a7e7352b1
Sam Curren <telegramsam@gmail.com>
2020-06-23 08:10:06 -0600

    Fixed holder, added admin ports
    Signed-off-by: Sam Curren <telegramsam@gmail.com>

--- startup.sh
+++ startup.sh
@@ -6,10 +6,12 @@ aca-py start \
     --auto-accept-requests --auto-ping-connection \
     --auto-respond-credential-proposal --auto-respond-credential-offer --auto-respond-credential-request --auto-store-credential \
     --auto-respond-presentation-proposal --auto-respond-presentation-request --auto-verify-presentation \
+    --preserve-exchange-records \
     --invite --invite-role admin --invite-label "$AGENT_NAME (admin)" \
     --genesis-url https://raw.githubusercontent.com/sovrin-foundation/sovrin/master/sovrin/pool_transactions_sandbox_genesis \
     --wallet-type indy \
     --plugin acapy_plugin_toolbox \
+    --admin 0.0.0.0 $ADMIN_PORT --admin-insecure-mode \
     --debug-connections \
     --debug-credentials \
     --debug-presentations \
