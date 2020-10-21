7bd71103979adcd6410072a7b5dfe5d6628f009e
Adam Burdett <burdettadam@gmail.com>
2020-10-21 16:50:09 -0600

    sendMediationRequestHandler
    Signed-off-by: Adam Burdett <burdettadam@gmail.com>

--- acapy_plugin_toolbox/routing.py
+++ acapy_plugin_toolbox/routing.py
@@ -8,9 +8,7 @@ from marshmallow import fields
 
 from aries_cloudagent.config.injection_context import InjectionContext
 from aries_cloudagent.core.protocol_registry import ProtocolRegistry
-from aries_cloudagent.connections.models.connection_record import (
-    ConnectionRecord
-)
+
 from aries_cloudagent.messaging.base_handler import (
     BaseHandler, BaseResponder, RequestContext
 )
@@ -21,7 +19,6 @@ from aries_cloudagent.messaging.models.base_record import (
     BaseRecord, BaseRecordSchema
 )
 from aries_cloudagent.messaging.valid import INDY_ISO8601_DATETIME
-from aries_cloudagent.protocols.connections.v1_0.manager import ConnectionManager
 from aries_cloudagent.protocols.problem_report.v1_0.message import ProblemReport
 from aries_cloudagent.storage.error import StorageNotFoundError
 
@@ -29,16 +26,21 @@ from aries_cloudagent.protocols.routing.v1_0.messages.route_update_request impor
 from aries_cloudagent.protocols.routing.v1_0.models.route_update import RouteUpdate
 from aries_cloudagent.protocols.routing.v1_0.messages.route_query_request import RouteQueryRequest
 
+from aries_cloudagent.protocols.coordinate_mediation.v1_0.messages.mediation_request import MediationRequest
+
 from .util import (
     generate_model_schema, admin_only, timestamp_utc_iso, datetime_from_iso
 )
 
 ADMIN_PROTOCOL_URI = "https://github.com/hyperledger/" \
     "aries-toolbox/tree/master/docs/admin-routing/0.1"
+
 SEND_UPDATE = f"{ADMIN_PROTOCOL_URI}/send_update"
+SEND_MEDIATION_REQUEST = f"{ADMIN_PROTOCOL_URI/send_mediation_request}"
 
 MESSAGE_TYPES = {
     SEND_UPDATE: 'acapy_plugin_toolbox.routing.SendUpdate',
+    SEND_MEDIATION_REQUEST: 'acapy_plugin_toolbox.routing.SendMediationRequest',
 }
 
 
@@ -46,7 +48,7 @@ async def setup(
         context: InjectionContext,
         protocol_registry: ProblemReport = None
 ):
-    """Setup the basicmessage plugin."""
+    """Setup the routing plugin."""
     if not protocol_registry:
         protocol_registry = await context.inject(ProtocolRegistry)
     protocol_registry.register_message_types(
@@ -83,5 +85,34 @@ class SendUpdateHandler(BaseHandler):
         # TODO make sure connection_id is valid, fail gracefully
         await responder.send(
             update_msg,
+            connection_id=context.message.connection_id,
+        )
+
+
+SendMediationRequest, SendMediationRequestSchema = generate_model_schema(
+    name='SendMediationRequest',
+    handler='acapy_plugin_toolbox.routing.SendMediationRequestHandler',
+    msg_type=SEND_MEDIATION_REQUEST,
+    schema={
+        'connection_id': fields.Str(required=True),
+        'mediator_terms': fields.List(fields.Str(), required=False),
+        'recipient_terms': fields.List(fields.Str(), required=False),
+    }
+)
+
+
+class SendMediationRequestHandler(BaseHandler):
+    """Handler for sending mediation requests."""
+
+    @admin_only
+    async def handle(self, context: RequestContext, responder: BaseResponder):
+        mediation_requests = MediationRequest(
+            mediator_terms = context.message.mediator_terms,
+            recipient_terms = context.message.recipient_terms,
+        )
+        
+        # TODO make sure connection_id is valid, fail gracefully
+        await responder.send(
+            mediation_requests,
             connection_id=context.message.connection_id,
         )
