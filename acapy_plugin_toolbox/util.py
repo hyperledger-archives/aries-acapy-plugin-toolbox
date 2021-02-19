d4903b62a3629d32962f9229fe2b97b64a2764a5
Daniel Bluhm <dbluhm@pm.me>
2021-02-19 12:54:17 -0500

    feat: reduce handler boilerplate
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- acapy_plugin_toolbox/util.py
+++ acapy_plugin_toolbox/util.py
@@ -56,18 +56,16 @@ def require_role(role):
     """
     def _require_role(func):
         @functools.wraps(func)
-        async def _wrapped(
-            handler,
-            context: RequestContext,
-            responder: BaseResponder
-        ):
+        async def _wrapped(*args):
+            context, *_ = [arg for arg in args if isinstance(arg, RequestContext)]
+            responder, *_ = [arg for arg in args if isinstance(arg, BaseResponder)]
             if context.connection_record:
                 session = await context.session()
                 group = await context.connection_record.metadata_get(
                     session, 'group'
                 )
                 if group == role:
-                    return await func(handler, context, responder)
+                    return await func(*args)
 
             report = ProblemReport(
                 explain_ltxt='This connection is not authorized to perform'
@@ -88,14 +86,11 @@ def admin_only(func):
 
 def expand_message_class(cls):
     """Class decorator for removing boilerplate of AgentMessages."""
+    # pylint: disable=protected-access
+
     if not hasattr(cls, "message_type"):
         raise ValueError(
             "Expected value message_type not found on class {}"
-            .format(cls.__name__)
-        )
-    if not hasattr(cls, "handler"):
-        raise ValueError(
-            "Expected value handler value not found on class {}"
             .format(cls.__name__)
         )
     if not hasattr(cls, "Fields") and not hasattr(cls, "fields_from"):
@@ -106,14 +101,17 @@ def expand_message_class(cls):
 
     cls.Meta = type(cls.__name__ + ".Meta", (), {
         "__module__": cls.__module__,
-        "handler_class": cls.handler,
         "message_type": cls.message_type,
         "schema_class": cls.__name__ + ".Schema"
     })
 
     fields = {}
     if hasattr(cls, "Fields"):
-        fields.update({var: getattr(cls.Fields, var) for var in vars(cls.Fields)})
+        fields.update({
+            var: getattr(cls.Fields, var)
+            for var in vars(cls.Fields)
+            if not var.startswith("__")
+        })
     if hasattr(cls, "fields_from"):
         fields.update(cls.fields_from._declared_fields)
 
@@ -121,6 +119,7 @@ def expand_message_class(cls):
         "__module__": cls.__module__,
         **fields
     })
+    cls.__slots__ = list(fields.keys())
     cls.Schema.Meta = type(cls.Schema.__name__ + ".Meta", (), {
         "__module__": cls.__module__,
         "model_class": cls
@@ -130,6 +129,16 @@ def expand_message_class(cls):
     if hasattr(cls, "protocol") and cls.protocol:
         cls.Meta.message_type = "{}/{}".format(cls.protocol, cls.message_type)
         cls._type = property(fget=lambda self: self.Meta.message_type)
+
+    if hasattr(cls, "handle"):
+        cls.Handler = handler(cls.handle)
+        cls._get_handler_class = lambda: cls.Handler
+    elif hasattr(cls, "handler"):
+        cls.Meta.handler_class = cls.handler
+    else:
+        cls.Handler = PassHandler
+        cls._get_handler_class = lambda: cls.Handler
+        cls.Meta.handler_class = PassHandler.load_path
 
     return cls
 
@@ -160,6 +169,7 @@ def expand_model_class(cls):
         "__module__": cls.__module__,
         **fields
     })
+    cls.__slots__ = list(fields.keys())
     cls.Schema.Meta = type(cls.Schema.__name__ + ".Meta", (), {
         "__module__": cls.__module__,
         "model_class": cls
@@ -173,6 +183,26 @@ def expand_model_class(cls):
     return cls
 
 
+def handler(func):
+    """Function decorator for creating Python handler classes."""
+
+    class Handler(BaseHandler):
+        __doc__ = func.__doc__
+        __name__ = func.__name__
+        __module__ = func.__module__
+
+        @property
+        @classmethod
+        def load_path(cls):
+            """Return load path for this handler."""
+            return f"{cls.__module__}.{cls.__name__}"
+
+        async def handle(self, context: RequestContext, responder: BaseResponder):
+            """Handle message."""
+            return await func(context.message, context, responder)
+
+    return Handler
+
 
 def generic_init(instance, **kwargs):
     """Initialize from kwargs into slots."""
@@ -181,6 +211,12 @@ def generic_init(instance, **kwargs):
         if slot in kwargs:
             del kwargs[slot]
     super(type(instance), instance).__init__(**kwargs)
+
+
+def with_generic_init(cls):
+    """Class decorator for adding generic init method."""
+    cls.__init__ = generic_init
+    return cls
 
 
 def generate_model_schema(  # pylint: disable=protected-access
@@ -261,6 +297,12 @@ def generate_model_schema(  # pylint: disable=protected-access
 
 class PassHandler(BaseHandler):
     """Handler for messages requiring no handling."""
+
+    @property
+    @classmethod
+    def load_path(cls):
+        """Return the load path of this handler."""
+        return f"{cls.__module__}.{cls.__name__}"
 
     async def handle(self, context: RequestContext, _responder):
         """Handle messages require no handling."""
