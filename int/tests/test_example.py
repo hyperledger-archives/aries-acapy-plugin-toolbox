4cade3f4a82376243f235ef37ae5d87861514f9d
Char Howland <60200853+cjhowland@users.noreply.github.com>
2021-06-10 11:16:18 -0700

    Task/integration tests (#61)
    * feat: add integration test base
    
    Signed-off-by: Char <char@indicio.tech>
    
    * chore: remove test_conftest.py after renaming
    
    Signed-off-by: Char <char@indicio.tech>

--- int/tests/test_example.py
+++ int/tests/test_example.py
@@ -6,16 +6,3 @@ def test_emptytest1():
 
 def test_emptytest2():
     assert 1+1 == 2
-
-def test_create_invitation():
-    pass
-
-    # {
-    #     "@type": "https://github.com/hyperledger/aries-toolbox/tree/master/docs/admin-invitations/0.1/create",
-    #     "alias": "Invitation I sent to Alice",
-    #     "label": "Bob"
-    #     "group": "admin",
-    #     "auto_accept": true,
-    #     "multi_use": true,
-    #     "mediation_id": "42a1f1c9-b463-4f59-8385-2e2f7b70466a"
-    # }
