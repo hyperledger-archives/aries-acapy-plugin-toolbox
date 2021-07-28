780bb2ac301b683a3e4ddfc4637c33c98b765051
Daniel Bluhm <dbluhm@pm.me>
2021-07-28 13:59:56 -0400

    fix: minor fixes in basicmessage tests
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>
    Signed-off-by: Char Howland <char@indicio.tech>

--- int/tests/test_basicmessage.py
+++ int/tests/test_basicmessage.py
@@ -1,92 +1,100 @@
 """Basic Message Tests"""
 import asyncio
+from acapy_backchannel.models.send_message import SendMessage
 import pytest
 from aries_staticagent import StaticConnection, utils
+from acapy_backchannel.api.basicmessage import send_basicmessage
 
 
-@pytest.mark.asyncio
-async def test_send(connection: StaticConnection, connection_id: str):
-    """Test send message"""
-    with connection.next() as future_recip_message:
-        sent_message = await asyncio.wait_for(
-            connection.send_and_await_reply_async(
-                {
-                    "@type": "https://github.com/hyperledger/aries-toolbox/tree/master/docs/admin-basicmessage/0.1/send",
-                    "connection_id": connection_id,
-                    "content": "Your hovercraft is full of eels.",
-                },
-                return_route="all",
-            ),
-            timeout=60,
+@pytest.fixture
+async def test_messages(connection, connection_id):
+    for i in range(6):
+        # This must be done by message or else the messages will not be recorded.
+        # await send_basicmessage.asyncio(
+        #     client=backchannel,
+        #     conn_id=connection_id,
+        #     json_body=SendMessage(content="Test Message #{}".format(i))
+        # )
+
+        # send_and_await_reply_async used instead of send_async to capture "sent"
+        # message so it doesn't clog up echo agent's queue
+        _ = await connection.send_and_await_reply_async(
+            {
+                "@type": "https://github.com/hyperledger/aries-toolbox/tree/master/docs/admin-basicmessage/0.1/send",
+                "connection_id": connection_id,
+                "content": "Test Message #{}".format(i),
+            },
         )
-        recip_message = await asyncio.wait_for(future_recip_message, 60)
-    assert (
-        recip_message["@type"]
-        == "https://github.com/hyperledger/aries-toolbox/tree/master/docs/admin-basicmessage/0.1/sent"
-    )
-    assert recip_message["message"]["content"] == "Your hovercraft is full of eels."
-    # TODO add proper backchannel for clearing messages
+
+
+@pytest.fixture(autouse=True)
+async def clear_messages(connection):
+    yield
     await connection.send_and_await_reply_async(
         {
             "@type": "https://github.com/hyperledger/aries-toolbox/tree/master/docs/admin-basicmessage/0.1/delete",
-        }
+        },
+        timeout=5,
     )
+
+
+@pytest.mark.asyncio
+async def test_send(
+    connection: StaticConnection, connection_id: str, asynchronously_recevied_messages
+):
+    """Test send message"""
+    sent_message = await connection.send_and_await_reply_async(
+        {
+            "@type": "https://github.com/hyperledger/aries-toolbox/tree/master/docs/admin-basicmessage/0.1/send",
+            "connection_id": connection_id,
+            "content": "Your hovercraft is full of eels.",
+        },
+    )
+    [recip_message] = await asynchronously_recevied_messages()
+    assert (
+        sent_message["@type"]
+        == "https://github.com/hyperledger/aries-toolbox/tree/master/docs/admin-basicmessage/0.1/sent"
+    )
+    assert (
+        recip_message["@type"]
+        == "did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/basicmessage/1.0/message"
+    )
+    assert recip_message["content"] == "Your hovercraft is full of eels."
 
 
 @pytest.mark.asyncio
 async def test_new(connection: StaticConnection):
     """Test new message notification"""
-    new_response = await connection.send_and_await_reply_async(
+    # "new" message notifications are sent only over sessions.
+    # This call must be done as a send_and_await_reply_async
+    new_message = await connection.send_and_await_reply_async(
         {
             "@type": "https://didcomm.org/basicmessage/1.0/message",
             "~l10n": {"locale": "en"},
             "sent_time": utils.timestamp(),
             "content": "Your hovercraft is full of eels.",
         },
-        return_route="all",
+        timeout=10,
     )
     assert (
-        new_response["@type"]
+        new_message["@type"]
         == "https://github.com/hyperledger/aries-toolbox/tree/master/docs/admin-basicmessage/0.1/new"
     )
-    assert new_response["message"]["content"] == "Your hovercraft is full of eels."
-    # Delete messages to clear the state between tests
-    await connection.send_and_await_reply_async(
-        {
-            "@type": "https://github.com/hyperledger/aries-toolbox/tree/master/docs/admin-basicmessage/0.1/delete",
-        }
-    )
+    assert new_message["message"]["content"] == "Your hovercraft is full of eels."
 
 
 @pytest.mark.asyncio
 async def test_get(connection: StaticConnection, connection_id: str):
     """Send multiple messages and verify that the proper count and content appears in messages list"""
-    with connection.next() as future_recip_message:
-        sent_message = await asyncio.wait_for(
-            connection.send_and_await_reply_async(
-                {
-                    "@type": "https://github.com/hyperledger/aries-toolbox/tree/master/docs/admin-basicmessage/0.1/send",
-                    "connection_id": connection_id,
-                    "content": "Are you suggesting coconuts migrate?",
-                },
-                return_route="all",
-            ),
-            timeout=60,
+    test_content = ("Are you suggesting coconuts migrate?", "'Tis but a flesh wound.")
+    for content in test_content:
+        _ = await connection.send_and_await_reply_async(
+            {
+                "@type": "https://github.com/hyperledger/aries-toolbox/tree/master/docs/admin-basicmessage/0.1/send",
+                "connection_id": connection_id,
+                "content": content,
+            }
         )
-        recip_message = await asyncio.wait_for(future_recip_message, 60)
-    with connection.next() as future_recip_message:
-        sent_message = await asyncio.wait_for(
-            connection.send_and_await_reply_async(
-                {
-                    "@type": "https://github.com/hyperledger/aries-toolbox/tree/master/docs/admin-basicmessage/0.1/send",
-                    "connection_id": connection_id,
-                    "content": "'Tis but a flesh wound.",
-                },
-                return_route="all",
-            ),
-            timeout=60,
-        )
-        recip_message = await asyncio.wait_for(future_recip_message, 60)
     get_messages = await connection.send_and_await_reply_async(
         {
             "@type": "https://github.com/hyperledger/aries-toolbox/tree/master/docs/admin-basicmessage/0.1/get",
@@ -97,35 +105,15 @@ async def test_get(connection: StaticConnection, connection_id: str):
         == "https://github.com/hyperledger/aries-toolbox/tree/master/docs/admin-basicmessage/0.1/messages"
     )
     assert get_messages["count"] == 2
-    assert (
-        get_messages["messages"][1]["content"] == "Are you suggesting coconuts migrate?"
-    )
-    assert get_messages["messages"][0]["content"] == "'Tis but a flesh wound."
-    # Delete messages to clear the state between tests
-    await connection.send_and_await_reply_async(
-        {
-            "@type": "https://github.com/hyperledger/aries-toolbox/tree/master/docs/admin-basicmessage/0.1/delete",
-        }
-    )
+    assert get_messages["messages"][1]["content"] == test_content[0]
+    assert get_messages["messages"][0]["content"] == test_content[1]
 
 
 @pytest.mark.asyncio
-async def test_get_limit_offset(connection: StaticConnection, connection_id: str):
+async def test_get_limit_offset(
+    connection: StaticConnection, connection_id: str, test_messages
+):
     """Send multiple messages and verify that get returns the correct content according to the limit and offset"""
-    for i in range(6):
-        with connection.next() as future_recip_message:
-            sent_message = await asyncio.wait_for(
-                connection.send_and_await_reply_async(
-                    {
-                        "@type": "https://github.com/hyperledger/aries-toolbox/tree/master/docs/admin-basicmessage/0.1/send",
-                        "connection_id": connection_id,
-                        "content": "Test Message #{}".format(i),
-                    },
-                    return_route="all",
-                ),
-                timeout=60,
-            )
-            recip_message = await asyncio.wait_for(future_recip_message, 60)
     get_messages = await connection.send_and_await_reply_async(
         {
             "@type": "https://github.com/hyperledger/aries-toolbox/tree/master/docs/admin-basicmessage/0.1/get",
@@ -141,31 +129,11 @@ async def test_get_limit_offset(connection: StaticConnection, connection_id: str
     assert get_messages["messages"][0]["content"] == "Test Message #3"
     assert get_messages["messages"][1]["content"] == "Test Message #2"
     assert get_messages["messages"][2]["content"] == "Test Message #1"
-    # Delete messages to clear the state between tests
-    await connection.send_and_await_reply_async(
-        {
-            "@type": "https://github.com/hyperledger/aries-toolbox/tree/master/docs/admin-basicmessage/0.1/delete",
-        }
-    )
 
 
 @pytest.mark.asyncio
-async def test_delete(connection: StaticConnection, connection_id: str):
+async def test_delete(connection: StaticConnection, connection_id: str, test_messages):
     """Send multiple messages, delete them, and verify that the messages count is zero"""
-    for i in range(6):
-        with connection.next() as future_recip_message:
-            sent_message = await asyncio.wait_for(
-                connection.send_and_await_reply_async(
-                    {
-                        "@type": "https://github.com/hyperledger/aries-toolbox/tree/master/docs/admin-basicmessage/0.1/send",
-                        "connection_id": connection_id,
-                        "content": "Test Message #{}".format(i),
-                    },
-                    return_route="all",
-                ),
-                timeout=60,
-            )
-            recip_message = await asyncio.wait_for(future_recip_message, 60)
     delete_message = await connection.send_and_await_reply_async(
         {
             "@type": "https://github.com/hyperledger/aries-toolbox/tree/master/docs/admin-basicmessage/0.1/delete",
