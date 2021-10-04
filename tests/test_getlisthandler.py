7121e4fa7e01644b4ce285e778533850395aa84d
PeterStrob <peter@indicio.tech>
2021-10-04 13:10:49 -0700

    feat: added connections GetListHandler test
    Signed-off-by: PeterStrob <peter@indicio.tech>

---
+++ tests/test_getlisthandler.py
@@ -0,0 +1,44 @@
+from unittest.mock import patch
+
+import pytest
+from _pytest.fixtures import yield_fixture
+from aries_cloudagent.connections.models.conn_record import ConnRecord
+from aries_cloudagent.messaging.responder import MockResponder
+from asynctest import mock
+
+import acapy_plugin_toolbox.connections as con
+
+
+@pytest.fixture
+def message():
+    """Message fixture"""
+    yield con.GetList()
+
+
+@pytest.fixture
+def context(context, message):
+    """RequestContext fixture."""
+    context.message = message
+    yield context
+
+
+@pytest.fixture
+def responder():
+    """Responder fixture."""
+    return MockResponder()
+
+
+@pytest.mark.asyncio
+async def test_getlisthandler(context, responder):
+    """GetListHandler test.
+    
+    A unit test for the GetListHandler class.
+    """
+    handler = con.GetListHandler()
+    print(type(context))
+    with patch.object(ConnRecord, 'query', 
+    mock.CoroutineMock()) as mocked_query:
+        
+        await handler.handle(context, responder)
+        conn_list, _ = responder.messages[0]
+        assert isinstance(conn_list, con.List)
