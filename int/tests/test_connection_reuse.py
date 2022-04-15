4848d216094c7f670ec854f1ea4188011efadcbe
Char Howland <char@indicio.tech>
2022-04-14 19:52:14 -0600

    test: connection reuse with multiple invitations
    Signed-off-by: Char Howland <char@indicio.tech>

--- int/tests/test_connection_reuse.py
+++ int/tests/test_connection_reuse.py
@@ -76,8 +76,64 @@ async def test_connection_reuse_single_invitation(
     )
     connections_final = await get_connections.asyncio(client=backchannel)
     len_conn_final = len(connections_final.results)
-
-    print("Connections initial: ", connections_initial.results)
-    print(" ")
-    print("Connections final: ", connections_final.results)
     assert len_conn_initial == len_conn_final
+
+
+@pytest.mark.asyncio
+async def test_connection_reuse_multiple_invitations(
+    backchannel: Client,
+    echo: EchoClient,
+    echo_connection: ConnectionInfo,
+    endorser_did,
+):
+    invitation = await post_out_of_band_create_invitation.asyncio(
+        client=backchannel,
+        json_body=InvitationCreateRequest(
+            handshake_protocols=["did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/didexchange/1.0"],
+            use_public_did="true",
+        ),
+        auto_accept="true",
+    )
+    await echo.send_message(
+        echo_connection,
+        {
+            "@type": "https://github.com/hyperledger/aries-toolbox/tree/master/docs/admin-connections/0.1/receive-oob-invitation",
+            "invitation": invitation.invitation_url,
+            "auto_accept": True,
+        },
+    )
+    await echo.get_message(
+        echo_connection,
+        msg_type=(
+            "https://github.com/hyperledger/aries-toolbox/tree/master/docs/"
+            "admin-connections/0.1/connected"
+        ),
+    )
+    connections_initial = await get_connections.asyncio(client=backchannel)
+    num_active_connections_initial = len(
+        [conn.state for conn in connections_initial.results if conn.state == "active"]
+    )
+
+    # Send and receive another invitation
+    invitation = await post_out_of_band_create_invitation.asyncio(
+        client=backchannel,
+        json_body=InvitationCreateRequest(
+            handshake_protocols=["did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/didexchange/1.0"],
+            use_public_did="true",
+        ),
+        auto_accept="true",
+    )
+    await echo.send_message(
+        echo_connection,
+        {
+            "@type": "https://github.com/hyperledger/aries-toolbox/tree/master/docs/admin-connections/0.1/receive-oob-invitation",
+            "invitation": invitation.invitation_url,
+            "auto_accept": True,
+            "use_existing_connection": True,
+        },
+    )
+    connections_final = await get_connections.asyncio(client=backchannel)
+    num_active_connections_final = len(
+        [conn.state for conn in connections_final.results if conn.state == "active"]
+    )
+    assert num_active_connections_initial == num_active_connections_final
