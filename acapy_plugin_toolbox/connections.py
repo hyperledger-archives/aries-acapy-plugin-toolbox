ddf62071a8a1aefa2de82e31bdcde44d0e061598
Peter Strobel <peter@indicio.tech>
2022-08-11 12:33:16 -0700

    fix: fixed connection reuse flag functionality
    Signed-off-by: Peter Strobel <peter@indicio.tech>

--- acapy_plugin_toolbox/connections.py
+++ acapy_plugin_toolbox/connections.py
@@ -324,6 +324,7 @@ class ReceiveInvitationHandler(BaseHandler):
             invitation,
             auto_accept=context.message.auto_accept,
             mediation_id=context.message.mediation_id,
+            connection_reuse = context.message.use_existing_connection,
         )
         connection_resp = Connection(**conn_record_to_message_repr(connection))
         connection_resp.assign_thread_from(context.message)
